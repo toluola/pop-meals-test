838a054cc508c5243d0aaea4f4fd9c26268a7ab2
Tolulope Olaniyan <toluola7@gmail.com>
2022-06-13 13:46:08 +0100

    add styles to orderlist

--- src/Component/OrderListItem.jsx
+++ src/Component/OrderListItem.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './order.css'
 import moment from 'moment'
 
 export default function OrderListItem({ headers, orders }) {
@@ -9,9 +8,15 @@ export default function OrderListItem({ headers, orders }) {
     if (new Date(time) > new Date()) return <div style={{color: 'green'}}>Confirmed</div>
     if (new Date(time) < new Date()) return <div style={{color: 'yellow'}}>Delivered</div>
   }
+  const styles = {
+    width: calculateColWidth, 
+    background: '#FFFFFF', 
+    padding: '20px 10px', 
+    marginBottom: '2px' 
+  }
   return (
     <div style={{ padding: '50px 200px'}}>
-      <div style={{ display: 'flex', background: '#FFFFFF' }}>
+      <div style={{ display: 'flex', background: '#FFFFFF', padding: '10px', marginBottom: '2px' }}>
         {headers.map(head => (
           <div style={{width: calculateColWidth, fontWeight: 600}}>{head}</div>
         ))}
@@ -19,11 +24,11 @@ export default function OrderListItem({ headers, orders }) {
       <div>
         {orders.map(order => (
           <div style={{display: 'flex'}}>
-            <div style={{width: calculateColWidth}}>{orderStatus(order.arrives_at_utc)}</div>
-            <div style={{width: calculateColWidth}}>{`${moment(order.arrives_at_utc).format('dddd')}, ${moment(order.arrives_at_utc).format("DD-MM-YYYY")}`}</div>
-            <div style={{width: calculateColWidth}}>{moment(order.arrives_at_utc).format('LT')}</div>
-            <div style={{width: calculateColWidth}}>{`#${order.order_id}`}</div>
-            <div style={{width: calculateColWidth}}>{` RM ${order.total_paid}`}</div>
+            <div style={styles}>{orderStatus(order.arrives_at_utc)}</div>
+            <div style={styles}>{`${moment(order.arrives_at_utc).format('dddd')}, ${moment(order.arrives_at_utc).format("DD-MM-YYYY")}`}</div>
+            <div style={styles}>{moment(order.arrives_at_utc).format('LT')}</div>
+            <div style={styles}>{`#${order.order_id}`}</div>
+            <div style={styles}>{` RM ${order.total_paid}`}</div>
           </div>
         ))}
       </div>
